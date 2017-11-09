b642f5110790b40ed794b7cc7603bb8ee58383cf
sotails <lucashahn@comcast.net>
2017-11-09 15:00:33 -0500

    Fixes on Website
    Some general fixes

--- mysite/personal/forms.py
+++ mysite/personal/forms.py
@@ -1,7 +1,8 @@
+
 from django import forms
 
 class ItemForm(forms.Form):
-	item_type = forms.CharField(label='option', max_length=100)
+	item_type = forms.ChoiceField(choices=[('1','Item'),('2','Spell') ,('3','Weapon')])
 	item_name = forms.CharField(label='name', max_length=200)
 	item_description = forms.CharField(label='description',min_length=1)
 
